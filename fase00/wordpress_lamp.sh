178842b298812ff8e8da6935e03212e13aee0b7c
japsasir <58173215+japsasir@users.noreply.github.com>
2020-12-14 14:31:20 +0100

    Posicionando configuración de MySQL

--- fase00/wordpress_lamp.sh
+++ fase00/wordpress_lamp.sh
@@ -41,6 +41,19 @@ rm latest.tar.gz
 apt install mysql-server -y
 # Editamos el archivo de configuración de MySQL, modificando la línea (Loop/cualquiera) Es necesario que acepte conexiones de cualquier origen para que cumpla con el enunciado de la práctica.
 sed -i "s/127.0.0.1/0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf 
+#git clone https://github.com/josejuansanchez/iaw-practica-lamp
+
+# Actualizamos la contraseña de root de MySQL
+#mysql -u root  <<< "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY '$DB_ROOT_PASSWD';"
+#mysql -u root -p$DB_ROOT_PASSWD <<< "FLUSH PRIVILEGES;"
+
+# Creamos usuario para la aplicación web y asignamos privilegios
+# mysql -u root -p$DB_ROOT_PASSWD <<< "CREATE USER 'lamp_user'@'%' IDENTIFIED BY '$DB_USU_PASSWD';"
+# mysql -u root -p$DB_ROOT_PASSWD <<< "GRANT ALL PRIVILEGES ON 'lamp_db'.* TO 'lamp_user'@'%';"
+# mysql -u root -p$DB_ROOT_PASSWD <<< "FLUSH PRIVILEGES;"
+
+# Introducimos la base de tados
+#mysql -u root -p$DB_ROOT_PASSWD < /home/ubuntu/iaw-practica-lamp/db/database.sql
 # Reiniciamos el servicio
 sudo /etc/init.d/mysql restart
 
@@ -67,16 +80,4 @@ sudo /etc/init.d/mysql restart
 # Clonamos el repositorio
 #cd /home/ubuntu
 #rm -rf iaw-practica-lamp 
-#git clone https://github.com/josejuansanchez/iaw-practica-lamp
 
-# Actualizamos la contraseña de root de MySQL
-#mysql -u root  <<< "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY '$DB_ROOT_PASSWD';"
-#mysql -u root -p$DB_ROOT_PASSWD <<< "FLUSH PRIVILEGES;"
-
-# Creamos usuario para la aplicación web y asignamos privilegios
-# mysql -u root -p$DB_ROOT_PASSWD <<< "CREATE USER 'lamp_user'@'%' IDENTIFIED BY '$DB_USU_PASSWD';"
-# mysql -u root -p$DB_ROOT_PASSWD <<< "GRANT ALL PRIVILEGES ON 'lamp_db'.* TO 'lamp_user'@'%';"
-# mysql -u root -p$DB_ROOT_PASSWD <<< "FLUSH PRIVILEGES;"
-
-# Introducimos la base de tados
-#mysql -u root -p$DB_ROOT_PASSWD < /home/ubuntu/iaw-practica-lamp/db/database.sql
